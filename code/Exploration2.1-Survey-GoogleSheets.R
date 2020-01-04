aab1c386677c50eacaeb9d029200609a0a3cc18b
Susan Vanderplas <srvanderplas@gmail.com>
2020-01-04 16:14:30 -0600

    change qnum

--- code/Exploration2.1-Survey-GoogleSheets.R
+++ code/Exploration2.1-Survey-GoogleSheets.R
@@ -8,9 +8,9 @@ q10 %>%
   ggtitle("Question 10 Responses")
 
 
-q18 <- read_sheet("https://docs.google.com/spreadsheets/d/1_snzmHEUdApqIqm0AtOuJPJKqHeYcWDWu2fRQb-0ki8/", sheet = 2)
+q17 <- read_sheet("https://docs.google.com/spreadsheets/d/1_snzmHEUdApqIqm0AtOuJPJKqHeYcWDWu2fRQb-0ki8/", sheet = 2)
 
-q18 %>%
+q17 %>%
   set_names(c("time", "avg_word_length")) %>%
   ggplot(aes(x = avg_word_length)) + geom_dotplot(binwidth = 0.25, origin = 1, method = "histodot") +
   ggtitle("Question 18 Responses")
