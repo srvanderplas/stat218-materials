8fe941907b9436a35cc03eb7ade01fc8ff7a38ef
Susan Vanderplas <srvanderplas@gmail.com>
2020-01-21 09:51:49 -0600

    change slide scaling

--- slides/all_slides_to_pdf.R
+++ slides/all_slides_to_pdf.R
@@ -5,8 +5,8 @@ html_to_pdf <- function(x) {
   xpdf <- stringr::str_replace(x, "html", "pdf")
   # webshot(x, file = xpdf, delay = 2)
   pagedown::chrome_print(x, xpdf)
-  system(paste0("pdfnup --nup 2x2 --frame true --noautoscale false --scale 0.95 --suffix 4 ", xpdf))
-  system(paste0("pdfnup --nup 1x2 --frame true --noautoscale false --scale 0.95 --no-landscape --suffix 2 ", xpdf))
+  system(paste0("pdfnup --nup 2x2 --frame true --noautoscale false --scale 0.92 --suffix 4 ", xpdf))
+  system(paste0("pdfnup --nup 1x2 --frame true --noautoscale false --scale 0.92 --no-landscape --suffix 2 ", xpdf))
   system(paste0("pdfjam-slides3up --suffix 3 ", xpdf))
 }
 
