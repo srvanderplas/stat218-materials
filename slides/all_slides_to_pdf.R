3636bc2fc4c05ed67cbf964c3a4735fc49660ff6
Susan Vanderplas <srvanderplas@gmail.com>
2020-01-21 10:25:50 -0600

    letter paper

--- slides/all_slides_to_pdf.R
+++ slides/all_slides_to_pdf.R
@@ -1,13 +1,15 @@
 setwd("slides/")
 html_files <- list.files(".", "html")
 
+html_files <- html_files[!grepl("index", html_files)]
+
 html_to_pdf <- function(x) {
   xpdf <- stringr::str_replace(x, "html", "pdf")
   # webshot(x, file = xpdf, delay = 2)
   pagedown::chrome_print(x, xpdf)
-  system(paste0("pdfnup --nup 2x2 --frame true --noautoscale false --scale 0.92 --suffix 4 ", xpdf))
-  system(paste0("pdfnup --nup 1x2 --frame true --noautoscale false --scale 0.92 --no-landscape --suffix 2 ", xpdf))
-  system(paste0("pdfjam-slides3up --suffix 3 ", xpdf))
+  system(paste0("pdfnup --nup 2x2 --frame true --paper letterpaper --noautoscale false --scale 0.92 --suffix 4 ", xpdf))
+  system(paste0("pdfnup --nup 1x2 --frame true --paper letterpaper --noautoscale false --scale 0.92 --no-landscape --suffix 2 ", xpdf))
+  system(paste0("pdfjam-slides3up --suffix 3 --paper letterpaper ", xpdf))
 }
 
 purrr::map(html_files, html_to_pdf)
