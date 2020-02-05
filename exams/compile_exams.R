880722b6b7e2d78864d8f5bd34dca5f1f259e2b3
Susan Vanderplas <srvanderplas@gmail.com>
2020-02-05 16:02:43 -0600

    Exam compilation script

---
+++ exams/compile_exams.R
@@ -0,0 +1,26 @@
+# Compile the exams
+
+library(tidyverse)
+setwd("exams")
+# Set up each exam command separately because versions and parameters may be different.
+crossing(version = 1:4, key = c(T, F)) %>%
+  purrr::pwalk(., function(version, key) {
+    rmarkdown::render(
+      "exam1.Rmd",
+      output_file = sprintf("exam1%s_version-%d.pdf",
+                            ifelse(key, "_key", ""), version),
+      params = list(key = key, version = version))
+  })
+
+# Compile the practice exams
+setwd("exam-practice/")
+tibble(key = c(F, T)) %>%
+  purrr::pwalk(., function(key) {
+    rmarkdown::render(
+      "exam1-practice.Rmd",
+      output_file = sprintf("exam1-practice%s.pdf",
+                            ifelse(key, "_key", "")),
+      params = list(key = key))
+  })
+
+rmarkdown::render("index.Rmd")
