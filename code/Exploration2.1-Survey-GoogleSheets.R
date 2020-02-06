95e4766d3b7a522fad2c82baa901946d7b4bab67
Susan Vanderplas <srvanderplas@gmail.com>
2020-02-06 16:09:58 -0600

    Add statistics of note.

--- code/Exploration2.1-Survey-GoogleSheets.R
+++ code/Exploration2.1-Survey-GoogleSheets.R
@@ -4,13 +4,21 @@ q10 <- read_sheet("https://docs.google.com/spreadsheets/d/1_snzmHEUdApqIqm0AtOuJ
 
 q10 %>%
   set_names(c("time", "avg_word_length")) %>%
-  ggplot(aes(x = avg_word_length)) + geom_dotplot(binwidth = 0.25, origin = 1, method = "histodot") +
-  ggtitle("Question 10 Responses")
+  ggplot(aes(x = avg_word_length)) +
+  geom_dotplot(binwidth = 0.25, origin = 1, method = "histodot") +
+  ggtitle("Question 10 Responses") +
+  annotate("text", x = Inf, y = Inf, hjust = 1.1, vjust = 1, label = sprintf("Mean word length: %.2f\nProportion > 4.29: %.2f",
+                                                                             mean(q10$`What is the average word length in your sample?`), mean(q10$`What is the average word length in your sample?`>4.29)))
 
 
-q17 <- read_sheet("https://docs.google.com/spreadsheets/d/1_snzmHEUdApqIqm0AtOuJPJKqHeYcWDWu2fRQb-0ki8/", sheet = 2)
+q17 <- read_sheet("https://docs.google.com/spreadsheets/d/1_snzmHEUdApqIqm0AtOuJPJKqHeYcWDWu2fRQb-0ki8/", sheet = 2) %>% na.omit()
 
 q17 %>%
+  # na.omit() %>%
   set_names(c("time", "avg_word_length")) %>%
   ggplot(aes(x = avg_word_length)) + geom_dotplot(binwidth = 0.25, origin = 1, method = "histodot") +
-  ggtitle("Question 18 Responses")
+  ggtitle("Question 18 Responses") +
+  annotate("text", x = Inf, y = Inf, hjust = 1.1, vjust = 1, label = sprintf("Mean word length: %.2f\nProportion > 4.29: %.2f",
+                                                                             mean(q17$`What is the average word length in your sample?`), mean(q17$`What is the average word length in your sample?`>4.29)))
+  # coord_cartesian(xlim = c(0, 10))
+
