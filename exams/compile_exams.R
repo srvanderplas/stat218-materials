120730172f3fdc3095096378fd149ba24b6dfe14
Susan Vanderplas <srvanderplas@gmail.com>
2020-02-25 11:08:33 -0600

    Set up for exams as its own git repo. This will (somewhat) break practice exam compilation.

--- exams/compile_exams.R
+++ exams/compile_exams.R
@@ -1,14 +1,25 @@
 # Compile the exams
 
 library(tidyverse)
-setwd(here::here("exams"))
+# setwd(here::here("exams"))
 # Set up each exam command separately because versions and parameters may be different.
-setwd(here::here("exams/exam1"))
+# setwd(here::here("exams/exam1"))
+# crossing(version = 1:4, key = c(T, F)) %>%
+#   purrr::pwalk(., function(version, key) {
+#     rmarkdown::render(
+#       "exam1.Rmd",
+#       output_file = sprintf("exam1%s_version-%d.pdf",
+#                             ifelse(key, "_key", ""), version),
+#       params = list(key = key, version = version))
+#   })
+
+# Set up each exam command separately because versions and parameters may be different.
+setwd(here::here("exam2"))
 crossing(version = 1:4, key = c(T, F)) %>%
   purrr::pwalk(., function(version, key) {
     rmarkdown::render(
-      "exam1.Rmd",
-      output_file = sprintf("exam1%s_version-%d.pdf",
+      "exam2.Rmd",
+      output_file = sprintf("exam2%s_version-%d.pdf",
                             ifelse(key, "_key", ""), version),
       params = list(key = key, version = version))
   })
@@ -30,5 +41,6 @@ setwd(here::here())
 rmarkdown::render("index.Rmd") # Render complete index
 
 # Clean up extra files
+file.remove(list.files(here::here("exam2"), ".log", full.names = T, recursive = T))
 file.remove(list.files(here::here("exams/"), "tmp-pdfcrop", full.names = T, recursive = T))
 file.remove(list.files(here::here("exam_practice/"), "tmp-pdfcrop", full.names = T, recursive = T))
