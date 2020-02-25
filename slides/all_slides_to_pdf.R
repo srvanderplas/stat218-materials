c60d7543bea41f337f4cded75e1d3d1c03c6b2df
Susan Vanderplas <srvanderplas@gmail.com>
2020-02-25 08:18:42 -0600

    Updated pdf and html slides

--- slides/all_slides_to_pdf.R
+++ slides/all_slides_to_pdf.R
@@ -1,21 +1,37 @@
 setwd(here::here("slides/"))
-html_files <- list.files(".", "html")
+rmd_files <- list.files(".", "Rmd")
 
-html_files <- html_files[!grepl("index", html_files)]
+build_slides <- function(x, stop_at = ifelse(grepl("index", x), "html", "pdf")) {
+  x_base <- stringr::str_extract(x, "\\.((?:[Pp][Dd][Ff])|(?:[Hh][Tt][Mm][Ll])|(?:[Rr][Mm][Dd]))$")
+  x_base <- stringr::str_remove(x_base, "\\.")
 
-html_to_pdf <- function(x) {
-  xpdf <- stringr::str_replace(x, "html", "pdf")
-  xrmd <- stringr::str_replace(x, "html", "Rmd")
-  if (!file.exists(xrmd)) return(paste0("ERROR printing ", x, " Rmd file does not exist"))
+  xhtml <- stringr::str_replace(x, x_base, "html")
+  xpdf <- stringr::str_replace(x, x_base, "pdf")
+  xrmd <- stringr::str_replace(x, x_base, "Rmd")
 
-  # Rerender if rmd modtime is more recent
-  if (file.mtime(xrmd) > file.mtime(x)) rmarkdown::render(input = xrmd, output_file = x)
+  if (!file.exists(xrmd)) {
+    warning(paste0("ERROR printing ", x, " Rmd file does not exist"))
+  } else {
+    # Rerender if rmd modtime is more recent or if HTML file doesn't exist
+    html_update <- ifelse(file.exists(xhtml), file.mtime(xhtml), -Inf)
+    if (file.mtime(xrmd) > file.mtime(x) | file.mtime(xrmd) > html_update) {
+      rmarkdown::render(input = xrmd, output_file = x)
+    }
+  }
 
-  # If PDF doesn't exist, that's ok
+  # If HTML still doesn't exist, quit
+  if (!file.exists(xhtml)) {
+    warning(paste0("ERROR printing ", xhtml, " html file does not exist"))
+    return()
+  }
+  # If we're stopping at HTML, quit
+  if (stop_at == "html") return()
+
+  # If PDF doesn't exist, create it
   pdf_update <- ifelse(file.exists(xpdf), file.mtime(xpdf), -Inf)
-  if (file.mtime(x) > pdf_update) {
-    res <- try(pagedown::chrome_print(x, xpdf))
-    if ("try-error" %in% class(res)) warning(paste("PDF build of ", x, " failed")) else {
+  if (file.mtime(xhtml) > pdf_update) {
+    res <- try(pagedown::chrome_print(xhtml, xpdf))
+    if ("try-error" %in% class(res)) warning(paste("PDF build of ", xhtml, " failed")) else {
       system(paste0("pdfnup --nup 2x2 --frame true --paper letterpaper --noautoscale false --scale 0.92 --suffix 4 ", xpdf))
       system(paste0("pdfnup --nup 1x2 --frame true --paper letterpaper --noautoscale false --scale 0.92 --no-landscape --suffix 2 ", xpdf))
       system(paste0("pdfjam-slides3up --suffix 3 --paper letterpaper ", xpdf))
@@ -23,14 +39,8 @@ html_to_pdf <- function(x) {
   }
 }
 
-# mod_date_Rmd <- file.mtime(str_replace(html_files, "html", "Rmd"))
-# mod_date_html <- file.mtime(html_files)
-# mod_date_pdf <- file.mtime(str_replace(html_files, "html", "pdf"))
-#
-# regen <- (mod_date_html >= mod_date_pdf) | is.na(mod_date_pdf)
 
-purrr::map(html_files, html_to_pdf)
-rmarkdown::render("index.Rmd")
+purrr::map(rmd_files, build_slides) %>% unlist()
 
 git2r::add(path = list.files(here::here("slides"), ".pdf$", full.names = T))
 git2r::add(path = list.files(here::here("slides"), ".html$", full.names = T))
